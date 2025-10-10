fb3a142c845d815f68ef2a983ae90432e0ae7e83
fluidvanadium <fluidvanadium@zingolabs.org>
2025-10-10 05:55:48 +0000

    cargo fmt

--- zingo_common_components/src/protocol/activation_heights/for_test.rs
+++ zingo_common_components/src/protocol/activation_heights/for_test.rs
@@ -1,16 +1,19 @@
 use zebra_chain::parameters;
 
 /// Get the default all nu activated at 1, Network
-#[must_use] pub fn current_nus_configured_in_block_one_regtest_net() -> parameters::Network {
+#[must_use]
+pub fn current_nus_configured_in_block_one_regtest_net() -> parameters::Network {
     parameters::Network::new_regtest(all_height_one_nus())
 }
 
 /// Get sequentially activated (1,2,3,4,5,6,7,8) nus network
-#[must_use] pub fn nus_configured_in_sequence_regtest_net() -> parameters::Network {
+#[must_use]
+pub fn nus_configured_in_sequence_regtest_net() -> parameters::Network {
     parameters::Network::new_regtest(sequential_height_nus())
 }
 
-#[must_use] pub fn sequential_height_nus() -> parameters::testnet::ConfiguredActivationHeights {
+#[must_use]
+pub fn sequential_height_nus() -> parameters::testnet::ConfiguredActivationHeights {
     parameters::testnet::ConfiguredActivationHeights {
         before_overwinter: Some(1),
         overwinter: Some(2),
@@ -25,7 +28,8 @@ use zebra_chain::parameters;
         nu7: None,
     }
 }
-#[must_use] pub fn all_height_one_nus() -> parameters::testnet::ConfiguredActivationHeights {
+#[must_use]
+pub fn all_height_one_nus() -> parameters::testnet::ConfiguredActivationHeights {
     parameters::testnet::ConfiguredActivationHeights {
         before_overwinter: Some(1),
         overwinter: Some(1),
